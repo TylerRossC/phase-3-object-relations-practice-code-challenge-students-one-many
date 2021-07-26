0c2a85baa19d11329854fb78c0f3218f27474a0f
tyler <tylercharawell@gmail.com>
2021-07-25 19:47:21 -0700

    Completed assignment

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,29 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students
+
+    def add_student(name, age)
+        student = Student.create(name: name, age: age, cohort_id: self.id)
+    end
+
+    def average_age
+        # self.students.average(:age)
+        students_ages = self.students.map{|student| student.age} # getting ages of all students 
+        total_years = students_ages.sum # summing all ages
+        total_years / students_ages.length.to_f # dividing by number of students to get average
+    end
+
+    def total_students
+        self.students.count
+    end
+
+    def self.biggest
+        cohort_size = self.all.map{|cohort| cohort.students.count}
+        idx = cohort_size.index(cohort_size.max())
+        self.all[idx]
+    end
+
+    def self.sort_by_mod
+        self.all.order(:current_mod)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,29 +4,22 @@ Student.destroy_all
 
 # This will create 5 cohorts using a random date
 puts "Seeding cohorts..."
-c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+10.times do
+  Cohort.create(
+  name: Faker::Name.first_name,
   current_mod: rand(1..5)
-)
-c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
-c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
-  current_mod: rand(1..5)
-)
+  )
+end
 
 puts "Seeding students..."
 # TODO: create seed data for students
 # check your schema and make sure to create students with all the necessary data
+50.times do 
+  Student.create(
+    name: Faker::Name.name, 
+    age:(18..100), 
+    cohort_id:rand(1..10)
+    )
+end
 
 puts "Done!"
